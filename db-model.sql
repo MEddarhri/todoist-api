101118ca8c39d88036a65b8311f8767f44213703
mokhtar-edd <emdprostudio@gmail.com>
2022-05-29 11:05:58 +0100

    adding db model

--- db-model.sql
+++ db-model.sql
@@ -1,5 +1,21 @@
-create extension "uuid-ossp"
+CREATE extension "uuid-ossp"
 
-create table users(
-    user_id uuid default uuid_genrate_uuid()
+CREATE TABLE users(
+    user_id uuid default uuid_genrate_uuid(),
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    password TEXT,
+    PRIMARY KEY (user_id)
 );
+
+CREATE TABLE todo(
+    todo_id uuid default uuid_genrate_uuid(),
+    user_id uuid ,
+    description TEXT NOT NULL,
+    title TEXT,
+    completed BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (todo_id)
+);
+
+
+
